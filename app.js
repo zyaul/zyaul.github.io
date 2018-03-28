ca18ff814af1269ea3da5d278690488fc5e121e8
Shekh Ziyaul <30418509+shekhzyaul@users.noreply.github.com>
2018-03-28 14:42:40 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const fs=require('fs');
-// const files=fs.readdirSync("./");
-// console.log(files);
-const files=fs.readdir("$",function(err,files){
-    if(err) console.log("Errorwa",err);
-    else console.log("result",files);
-});
